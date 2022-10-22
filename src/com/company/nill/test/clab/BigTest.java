e12a3e0e8a826de428ce10904ef7a5f2ebfdf953
shin <sars21@creativelab-inc.com>
2022-10-22 16:49:39 +0900

    Bigdecimal = add test

--- src/com/company/nill/test/clab/BigTest.java
+++ src/com/company/nill/test/clab/BigTest.java
@@ -10,17 +10,22 @@ public class BigTest {
         BigDecimal b = new BigDecimal("123");
         BigDecimal c = new BigDecimal(0);
         c = new BigDecimal(123);
-
+        BigDecimal d = BigDecimal.ZERO;
 
         int q = a.compareTo(new BigDecimal(0));
         int w = b.compareTo(new BigDecimal(0));
         int e = c.compareTo(new BigDecimal(0));
 
+        d = d.add(b);
+
+
         System.out.println(q);
         System.out.println(w);
         System.out.println(e);
 
-        System.out.print(a);
+        System.out.println(a);
+        System.out.println(d);
+
     }
 
 }
