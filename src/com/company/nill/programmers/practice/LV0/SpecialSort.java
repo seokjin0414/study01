5b1aeacad42758fee0b99baf7c3f7f6fb7fdd785
seokjin0414 <sars21@hanmail.net>
2023-06-09 18:11:21 +0900

    LV0 - 특이한 정렬

---
+++ src/com/company/nill/programmers/practice/LV0/SpecialSort.java
@@ -0,0 +1,28 @@
+package com.company.nill.programmers.practice.LV0;
+
+import java.util.*;
+
+public class SpecialSort {
+    class Solution {
+        public Object[] solution(int[] numlist, int n) {
+            ArrayList<Integer> answer = new ArrayList<Integer>();
+
+            for(int i=0; i<numlist.length; i++) {
+                answer.add(numlist[i]);
+            }
+
+            Collections.sort(answer, new Comparator<Integer>() {
+                @Override
+                public int compare(Integer a, Integer b) {
+                    if(Math.abs(a-n)!=Math.abs(b-n)) {
+                        return Math.abs(a-n)-Math.abs(b-n);
+                    } else {
+                        return b-a;
+                    }
+                }
+            });
+            return answer.toArray();
+        }
+    }
+
+}
