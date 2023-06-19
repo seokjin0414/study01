cc3e7e344e7b32c04845f524137ca89786e61b05
seokjin0414 <sars21@hanmail.net>
2023-06-19 18:34:27 +0900

    LV1 - 옹알이 (2)

---
+++ src/com/company/nill/programmers/practice/lv1/Babbling2.java
@@ -0,0 +1,32 @@
+package com.company.nill.programmers.practice.lv1;
+
+// 옹알이 (2)
+public class Babbling2 {
+    class Solution {
+        public static String[] list = {"aya", "ye", "woo", "ma"};
+        public int solution(String[] babbling) {
+            int answer = 0;
+            String[] list = {"aya", "ye", "woo", "ma"};
+
+            for (String s : babbling) {
+
+
+
+            }
+
+
+
+
+            return answer;
+        }
+        private int check(String babbling) {
+            int n = 0;
+
+
+
+
+            return n;
+        }
+
+    }
+}
