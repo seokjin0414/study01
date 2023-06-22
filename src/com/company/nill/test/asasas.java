ef500a7ac4bccbc7e64f59b55845937a87b631b1
seokjin0414 <sars21@hanmail.net>
2023-06-22 15:22:24 +0900

    my result

--- src/com/company/nill/test/asasas.java
+++ src/com/company/nill/test/asasas.java
@@ -2,8 +2,23 @@ package com.company.nill.test;
 
 public class asasas {
     public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
 
-        String a = "....###...###";
-        System.out.println(a.indexOf("#"));
+        for (int i = 1; i < 10; i++) {
+            if (i % 4 != 0) {
+                for (int j = 2; j < 10; j++) {
+                    if (j < 6) {
+                        sb.append(j + "x" + i + "=" + j * i + " ");
+                    } else {
+                        sb2.append(j + "x" + i + "=" + j * i + " ");
+                    }
+                }
+                sb.append("\n");
+                sb2.append("\n");
+            }
+        }
+        System.out.println(sb.append(sb2));
     }
 }
+
