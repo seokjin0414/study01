989f49e159289de05c5dadbfccb0e0ff76fd0419
seokjin0414 <sars21@hanmail.net>
2023-06-22 17:03:57 +0900

    LV 1 - 옹알이 (2)

--- src/com/company/nill/programmers/practice/lv1/Babbling2.java
+++ src/com/company/nill/programmers/practice/lv1/Babbling2.java
@@ -1,32 +1,46 @@
 package com.company.nill.programmers.practice.lv1;
 
-// 옹알이 (2)
+// LV 1 - 옹알이 (2)
 public class Babbling2 {
     class Solution {
         public static String[] list = {"aya", "ye", "woo", "ma"};
         public int solution(String[] babbling) {
             int answer = 0;
-            String[] list = {"aya", "ye", "woo", "ma"};
 
             for (String s : babbling) {
-
-
-
+                answer += check(s);
             }
-
-
-
 
             return answer;
         }
+
         private int check(String babbling) {
-            int n = 0;
+            String a = babbling;
+            String lastBabbling = "";
 
+            while (true) {
+                if (a.length() == 0) {
+                    return 1;
+                }
 
+                int n = 0;
+                for (String s : list) {
+                    if (a.indexOf(s) == 0) {
+                        if (s.equals(lastBabbling)) {
+                            break;
+                        }
+                        a = a.substring(s.length());
+                        lastBabbling = s;
+                        n++;
+                        break;
+                    }
+                }
 
-
-            return n;
+                if (n == 0)  {
+                    break;
+                }
+            }
+            return 0;
         }
-
     }
 }
