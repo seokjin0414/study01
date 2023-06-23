caf15fa7fb5b2c7ed884f03dcc1573c30d84073c
seokjin0414 <sars21@hanmail.net>
2023-06-23 17:57:09 +0900

    코테 -> coding-test-JAVA 로 이전

--- src/com/company/nill/programmers/practice/test.java
+++ src/com/company/nill/programmers/practice/test.java
@@ -3,10 +3,31 @@ package com.company.nill.programmers.practice;
 // 코드 작성용
 public class test {
     public static void main(String[] args) {
+        test t =new test();
+        t.solution("a", 1);
 
     }
 
+    public String solution(String s, int n) {
+        String lw = "abcdefghijklmnopqrstuvwxyz";
+        String up = lw.toUpperCase();
 
+        StringBuffer sb = new StringBuffer();
+
+        for (int i=0; i<s.length(); i++) {
+            if (Character.isLetter(s.charAt(i))) {
+                if (Character.isUpperCase(s.charAt(i))) {
+                    sb.append(up.charAt((up.indexOf(s.charAt(i)) + n) % up.length()));
+                } else {
+                    sb.append(lw.charAt((lw.indexOf(s.charAt(i)) + n) % lw.length()));
+                }
+            } else {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
 
 
 
