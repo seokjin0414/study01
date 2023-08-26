7388b6f91b6cd5d6fdc5ca2d4748aec6f9765724
seokjin0414 <sars21@hanmail.net>
2023-08-26 17:54:39 +0900

    function test

--- src/com/company/nill/test8to17/UsingSomething.java
+++ src/com/company/nill/test8to17/UsingSomething.java
@@ -1,6 +1,6 @@
 package com.company.nill.test8to17;
 
-import java.util.function.Function;
+import java.util.function.*;
 
 public class UsingSomething {
 
@@ -21,12 +21,24 @@ public class UsingSomething {
 
 
         Function<Integer, Integer> plus20 = (i) -> i + 20;
+        // input type ==  return type
+        UnaryOperator<Integer> plus20U = (i) -> 20;
         Function<Integer, Integer> multiply2 = (i) -> i * 2;
         Function<Integer, Integer> multiply2AndPlus20 = plus20.compose(multiply2);
 
         System.out.println(plus20.apply(2));
         System.out.println(multiply2.apply(2));
         System.out.println(multiply2AndPlus20.apply(6));
+
+        Consumer<Integer> plintT = System.out::println;
+        plintT.accept(10);
+
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println(get10.get());
+
+        Predicate<String> startWithShin = (s) -> s.startsWith("shin");
+        Predicate<Integer> isEven = (i) -> i % 2 == 0;
+
 
 
 
